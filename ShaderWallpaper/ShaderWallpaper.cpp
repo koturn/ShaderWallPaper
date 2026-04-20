c64a02645715a6f7d8448ad658aa91c0a8319e10
koturn <jeak.koutan.apple@gmail.com>
2026-04-21 03:27:46 +0900

    wip: not work

--- ShaderWallpaper/ShaderWallpaper.cpp
+++ ShaderWallpaper/ShaderWallpaper.cpp
@@ -59,6 +59,9 @@ class GlProg
 public:
 	GlProg()
 		: _program{ 0 }
+		, _vbo{ 0 }
+		, _ibo{ 0 }
+		, _aPosition{ 0 }
 		, _uTime{ 0 }
 		, _uMouse{ 0 }
 		, _uResolution{ 0 }
@@ -69,6 +72,9 @@ public:
 
 	GlProg(GLint program)
 		: _program(program)
+		, _vbo{ 0 }
+		, _ibo{ 0 }
+		, _aPosition{ glGetAttribLocation(program, "position")}
 		, _uTime{ glGetUniformLocation(program, "u_time") }
 		, _uMouse{ glGetUniformLocation(program, "u_mouse") }
 		, _uResolution{ glGetUniformLocation(program, "u_resolution") }
@@ -81,6 +87,21 @@ public:
 	GLint program() const
 	{
 		return _program;
+	}
+
+	GLint getOrCreateVbo(const GLfloat *vertices, size_t n)
+	{
+		return _vbo == 0 ? (_vbo = createVbo(vertices, n)) : _vbo;
+	}
+
+	GLint getOrCreateIbo(const GLushort *triangles, size_t n)
+	{
+		return _ibo == 0 ? (_ibo = createIbo(triangles, n)) : _ibo;
+	}
+
+	GLint aPosition() const
+	{
+		return _aPosition;
 	}
 
 	GLint uTime() const
@@ -109,12 +130,47 @@ public:
 	}
 private:
 	GLint _program;
+	GLint _vbo;
+	GLint _ibo;
+	GLint _aPosition;
 	GLint _uTime;
 	GLint _uMouse;
 	GLint _uResolution;
 	GLint _uFrameCount;
 	GLint _uPositionOffset;
 };
+
+
+struct Monitor
+{
+    RECT rc;
+    HWND hwnd;
+    HDC dc;
+    HGLRC rcGL;
+};
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch (msg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, msg, wParam, lParam);
+}
+
+void setupVsync(bool doVSync)
+{
+	if (strstr((char*)glGetString(GL_EXTENSIONS), "WGL_EXT_swap_control") == 0) {
+		return;
+	}
+
+	auto wglSwapIntervalEXT = (BOOL(WINAPI*)(int))wglGetProcAddress("wglSwapIntervalEXT");
+	if (wglSwapIntervalEXT != nullptr) {
+		wglSwapIntervalEXT(doVSync ? 1 : 0);
+	}
+}
 
 
 int main(int argc, char *argv[])
@@ -130,21 +186,37 @@ int main(int argc, char *argv[])
 			ReleaseDC(hWorkerW, hDC);
 		});
 
-		std::vector<RECT> rects;
-		// RECT rect;
-		// if (!GetWindowRect(hWorkerW, &rect)) {
-		// 	std::cerr << "GetWindowRect failed" << std::endl;
+		// std::vector<RECT> rects;
+		// // RECT rect;
+		// // if (!GetWindowRect(hWorkerW, &rect)) {
+		// // 	std::cerr << "GetWindowRect failed" << std::endl;
+		// // }
+		// // rects.emplace_back(rect);
+		// if (!EnumDisplayMonitors(NULL, NULL, +[](HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) -> BOOL {
+		// 		MONITORINFOEX monitorInfo;
+		// 		monitorInfo.cbSize = sizeof(monitorInfo);
+		// 		GetMonitorInfo(hMonitor, &monitorInfo);
+		// 		((std::vector<RECT>*)dwData)->emplace_back(monitorInfo.rcMonitor);
+		// 		return TRUE;
+		// 	}, (LPARAM)&rects)) {
+		// 	std::cerr << "EnumDisplayMonitors() failed" << std::endl;
+		// 	return 2;
 		// }
-		// rects.emplace_back(rect);
-		if (!EnumDisplayMonitors(NULL, NULL, +[](HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) -> BOOL {
-				MONITORINFOEX monitorInfo;
-				monitorInfo.cbSize = sizeof(monitorInfo);
-				GetMonitorInfo(hMonitor, &monitorInfo);
-				((std::vector<RECT>*)dwData)->emplace_back(monitorInfo.rcMonitor);
-				return TRUE;
-			}, (LPARAM)&rects)) {
-			std::cerr << "EnumDisplayMonitors() failed" << std::endl;
-			return 2;
+
+		std::vector<Monitor> mons;
+		auto ret = EnumDisplayMonitors(NULL, NULL, [](HMONITOR hMon, HDC, LPRECT, LPARAM p) -> BOOL {
+			std::vector<Monitor>* list = (std::vector<Monitor>*)p;
+			MONITORINFO mi = { sizeof(mi) };
+			if (!GetMonitorInfo(hMon, &mi)) {
+				return FALSE;
+			}
+			Monitor m = {};
+			m.rc = mi.rcMonitor;
+			list->push_back(m);
+			return TRUE;
+		}, (LPARAM)&mons);
+		if (!ret) {
+			return 1000;
 		}
 
 		PIXELFORMATDESCRIPTOR pfd = {
@@ -167,35 +239,110 @@ int main(int argc, char *argv[])
 			0,  // visiblemask
 			0   // damagemask
 		};
-		int format = ChoosePixelFormat(hDC, &pfd);
-		if (format == 0) {
+		int pf = ChoosePixelFormat(hDC, &pfd);
+		if (pf == 0) {
 			std::cerr << "ChoosePixelFormat() failed" << std::endl;
 			return 2;
 		}
 
-		if (!SetPixelFormat(hDC, format, &pfd)) {
-			std::cerr << "SetPixelFormat() failed" << std::endl;
-			return 3;
+		// if (!SetPixelFormat(hDC, pf, &pfd)) {
+		// 	std::cerr << "SetPixelFormat() failed" << std::endl;
+		// 	return 3;
+		// }
+
+		WNDCLASS wc = { 0 };
+		wc.lpfnWndProc = WndProc;
+		wc.hInstance = nullptr;
+		wc.lpszClassName = _T("ShaderWallPaper");
+		if (RegisterClass(&wc) == 0) {
+			std::cerr << "RegisterClass() failed" << std::endl;
+			return 1001;
 		}
 
-		auto hGlrc = wglCreateContext(hDC);
-		if (hGlrc == nullptr) {
-			std::cerr << "wglCreateContext() failed" << std::endl;
-			std::printf("LastError = 0x%08x\n", GetLastError());
-			return 2;
+		int index = 0;
+		for (auto& m : mons) {
+			int w = m.rc.right - m.rc.left;
+			int h = m.rc.bottom - m.rc.top;
+
+			m.hwnd = CreateWindowEx(
+				0,
+				_T("ShaderWallPaper"),
+				L"",
+				WS_CHILD | WS_VISIBLE,
+				m.rc.left,
+				m.rc.top,
+				w,
+				h,
+				hWorkerW,
+				0,
+				nullptr,
+				0);
+			if (m.hwnd == nullptr) {
+				std::cerr << "CreateWindowEx() failed: 0x" << std::hex << GetLastError() << std::endl;
+				return 100;
+			}
+
+			SetWindowLong(m.hwnd, GWL_EXSTYLE, GetWindowLong(m.hwnd, GWL_EXSTYLE) | WS_EX_TRANSPARENT | WS_EX_NOACTIVATE);
+
+			m.dc = GetDC(m.hwnd);
+			if (m.dc == nullptr) {
+				std::cerr << "GetDC() failed" << std::endl;
+				std::printf("LastError = 0x%08x\n", GetLastError());
+				return 2;
+			}
+			if (!SetPixelFormat(m.dc, pf, &pfd)) {
+				std::cerr << "SetPixelFormat() failed" << std::endl;
+				return 3;
+			}
+
+			m.rcGL = wglCreateContext(m.dc);
+			if (m.rcGL == nullptr) {
+				std::cerr << "wglCreateContext() failed" << std::endl;
+				std::printf("LastError = 0x%08x\n", GetLastError());
+				return 4;
+			}
+
+			if (index == 0) {
+				// for glewInit().
+				if (!wglMakeCurrent(m.dc, m.rcGL)) {
+					std::cerr << "wglMakeCurrent() failed" << std::endl;
+					std::printf("LastError = 0x%08x\n", GetLastError());
+					return 5;
+				}
+			} else {
+				if (!wglShareLists(mons[0].rcGL, m.rcGL)) {
+					std::cerr << "wglShareLists() failed" << std::endl;
+					std::printf("LastError = 0x%08x\n", GetLastError());
+				}
+			}
+
+			index++;
 		}
-		auto f2 = makeFinally([&] {
-			wglDeleteContext(hGlrc);
-			});
-		// make it the calling thread's current rendering context 
-		if (!wglMakeCurrent(hDC, hGlrc)) {
-			std::cerr << "wglMakeCurrent() failed" << std::endl;
-			std::printf("LastError = 0x%08x\n", GetLastError());
-			return 3;
+
+		if (glewInit() != GLEW_OK) {
+			std::cerr << "glewInit() failed" << std::endl;
+			return 4;
 		}
-		auto f3 = makeFinally([&] {
-			wglMakeCurrent(nullptr, nullptr);
-		});
+
+
+		// auto hGlrc = wglCreateContext(hDC);
+		// if (hGlrc == nullptr) {
+		// 	std::cerr << "wglCreateContext() failed" << std::endl;
+		// 	std::printf("LastError = 0x%08x\n", GetLastError());
+		// 	return 2;
+		// }
+		// auto f2 = makeFinally([&] {
+		// 	wglDeleteContext(hGlrc);
+		// 	});
+		// // make it the calling thread's current rendering context 
+		// if (!wglMakeCurrent(hDC, hGlrc)) {
+		// 	std::cerr << "wglMakeCurrent() failed" << std::endl;
+		// 	std::printf("LastError = 0x%08x\n", GetLastError());
+		// 	return 3;
+		// }
+		// auto f3 = makeFinally([&] {
+		// 	wglMakeCurrent(nullptr, nullptr);
+		// });
 
 		const char *targetDir = argc > 1 ? argv[1] : ".";
 		std::vector<std::pair<std::string, GlProg>> pathProgPairs;
@@ -214,11 +361,6 @@ int main(int argc, char *argv[])
 			}
 		});
 
-		if (glewInit() != GLEW_OK) {
-			std::cerr << "glewInit() failed" << std::endl;
-			return 4;
-		}
-
 		// call OpenGL APIs as desired ... 
 		// for (auto&& pair : pathProgPairs) {
 		// 	std::cout << "Compile " << pair.first << "..." << std::endl;
@@ -235,7 +377,7 @@ int main(int argc, char *argv[])
 		// 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, createIbo(kTriangles, sizeof(kTriangles) / sizeof(kTriangles[0])));
 		// }
 
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		// glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
 		auto isContinue = true;
 		std::thread th{ [&] {
@@ -245,58 +387,44 @@ int main(int argc, char *argv[])
 			std::cout << "FINISH" << std::endl;
 		} };
 
-
-		std::vector<std::pair<RECT, int>> rectIndexPairs;
+		// std::vector<std::pair<RECT, int>> rectIndexPairs;
+		std::vector<std::pair<Monitor, int>> monitorIndexPairs;
 		std::uniform_int_distribution<> dist(0, pathProgPairs.size() - 1);
 		std::mt19937 rnd{ std::random_device{}() };
-		for (const auto& rect : rects) {
-			while (!pathProgPairs.empty()) {
-				auto progIndex = dist(rnd);
-				try {
-					auto& pair = pathProgPairs[progIndex];
-					if (pair.second.program() == 0) {
-						std::cout << "Compile " << pair.first << "..." << std::endl;
-						auto program = buildProgram(pair.first.c_str());  // TODO
-						std::cout << "Compile " << pair.first << "... Done" << std::endl;
-						pair.second = GlProg{ program };
 
-						// Bind VBO.
-						auto attribLocation = glGetAttribLocation(program, "position");
-						glBindBuffer(GL_ARRAY_BUFFER, createVbo(kVertices, sizeof(kVertices) / sizeof(kVertices[0])));
-						glEnableVertexAttribArray(attribLocation);
-						glVertexAttribPointer(attribLocation, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-						// Bind IBO.
-						glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, createIbo(kTriangles, sizeof(kTriangles) / sizeof(kTriangles[0])));
-					}
-
-					rectIndexPairs.emplace_back(std::make_pair(rect, progIndex));
-					break;
-				} catch (std::exception& ex) {
-					std::cerr << ex.what() << std::endl;
-					pathProgPairs.erase(pathProgPairs.begin() + progIndex);
-					dist.param(std::uniform_int_distribution<>::param_type{0, (int)pathProgPairs.size() - 1});
-				}
-			}
-			if (pathProgPairs.empty()) {
-				return 5;  // No available program.
-			}
+		for (const auto& mon : mons) {
+			monitorIndexPairs.emplace_back(std::make_pair(mon, 0));
 		}
 
+		setupVsync(true);
+
+		auto needShuffle = true;
 		int frameCount = 0;
 		auto start = std::chrono::high_resolution_clock::now();
 		while (isContinue) {
-			glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-			glClearDepth(1.0f);
-			glClearStencil(0);
-			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+			const auto frameStart = std::chrono::high_resolution_clock::now();
+			const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(frameStart - start).count();
 
-			const auto end = std::chrono::high_resolution_clock::now();
-			const auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-			if (elapsed >= 10000) {
-				for (auto& riPair : rectIndexPairs) {
+			// Handle Window Messages.
+			MSG msg;
+			while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+				if (msg.message == WM_QUIT) {
+					return 0;
+				}
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+
+			if (elapsed >= 5000) {
+				needShuffle = true;
+				start = frameStart;
+			}
+
+			if (needShuffle) {
+				needShuffle = false;
+				for (auto& mon : monitorIndexPairs) {
 					while (!pathProgPairs.empty()) {
 						auto progIndex = dist(rnd);
-						std::cout << progIndex << std::endl;
 						try {
 							auto& pair = pathProgPairs[progIndex];
 							if (pair.second.program() == 0) {  // Need compile
@@ -304,20 +432,11 @@ int main(int argc, char *argv[])
 								auto program = buildProgram(pair.first.c_str());  // TODO
 								std::cout << "Compile " << pair.first << "... Done" << std::endl;
 								pair.second = GlProg{ program };
-
-								// Bind VBO.
-								auto attribLocation = glGetAttribLocation(program, "position");
-								glBindBuffer(GL_ARRAY_BUFFER, createVbo(kVertices, sizeof(kVertices) / sizeof(kVertices[0])));
-								glEnableVertexAttribArray(attribLocation);
-								glVertexAttribPointer(attribLocation, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-								// Bind IBO.
-								glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, createIbo(kTriangles, sizeof(kTriangles) / sizeof(kTriangles[0])));
 							}
 
-							riPair.second = progIndex;
+							mon.second = progIndex;
 							break;
-						}
-						catch (std::exception& ex) {
+						} catch (std::exception& ex) {
 							std::cerr << ex.what() << std::endl;
 							pathProgPairs.erase(pathProgPairs.begin() + progIndex);
 							dist.param(std::uniform_int_distribution<>::param_type{ 0, (int)pathProgPairs.size() - 1 });
@@ -327,34 +446,69 @@ int main(int argc, char *argv[])
 				if (pathProgPairs.empty()) {
 					return 6;  // No available program.
 				}
-
-				start = end;
 			}
 
-			for (const auto& riPair : rectIndexPairs) {
-				const auto& r = riPair.first;
+			for (const auto& miPair : monitorIndexPairs) {
+				const auto& mon = miPair.first;
+				const auto& r = mon.rc;
 				auto width = std::abs(r.right - r.left);
 				auto height = std::abs(r.bottom - r.top);
-				glViewport(r.left, r.top, width, height);
-				// glClear(GL_COLOR_BUFFER_BIT);
 
-				const auto& glProg = pathProgPairs[riPair.second].second;
+				// std::cout << "top = " << r.top
+				// 	<< ", left = " << r.left
+				// 	<< ", right = " << r.right
+				// 	<< ", bottom = " << r.bottom
+				// 	<< ", dc = " << mon.dc 
+				// 	<< ", rcGL = " << mon. rcGL
+				// 	<< "\n";
+
+				if (!wglMakeCurrent(mon.dc, mon.rcGL))
+				{
+					std::cerr << "wglMakeCurrent() failed" << std::endl;
+					std::fprintf(stderr, "LastError = 0x%08x\n", GetLastError());
+					return 3;
+				}
+
+				glViewport(0, 0, width, height);
+				glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+				glClearDepth(1.0f);
+				glClearStencil(0);
+				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+
+				auto& glProg = pathProgPairs[miPair.second].second;
 				glUseProgram(glProg.program());
+
+				glBindBuffer(GL_ARRAY_BUFFER, glProg.getOrCreateVbo(kVertices, sizeof(kVertices) / sizeof(kVertices[0])));
+				glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, glProg.getOrCreateIbo(kTriangles, sizeof(kTriangles) / sizeof(kTriangles[0])));
+
+				auto attribLocation = glProg.aPosition();
+				glEnableVertexAttribArray(attribLocation);
+				glVertexAttribPointer(attribLocation, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
 				glUniform1f(glProg.uTime(), static_cast<float>(static_cast<double>(elapsed) / 1000.0L));
 				glUniform2f(glProg.uMouse(), 0.0f, 0.0f);
 				glUniform2f(glProg.uResolution(), static_cast<float>(width), static_cast<float>(height));
 				glUniform1f(glProg.uFrameCount(), static_cast<float>(frameCount));
-				glUniform2f(glProg.uPositionOffset(), static_cast<float>(r.left), static_cast<float>(r.top));
+				// glUniform2f(glProg.uPositionOffset(), static_cast<float>(r.left), static_cast<float>(r.top));
+				glUniform2f(glProg.uPositionOffset(), static_cast<float>(0), static_cast<float>(0));
 
 				glDrawElements(GL_TRIANGLES, sizeof(kTriangles) / sizeof(kTriangles[0]), GL_UNSIGNED_SHORT, nullptr);
-			}
 
-			glFlush();
-			SwapBuffers(hDC);
+				if (!SwapBuffers(mon.dc)) {
+					std::cerr << "SwapBuffers() failed" << std::endl;
+				}
+			}
 
 			frameCount++;
 
-			std::this_thread::sleep_for(std::chrono::milliseconds{15});
+			const auto frameEnd = std::chrono::high_resolution_clock::now();
+			const auto sleepTime = std::chrono::microseconds{15 * 1000} - (frameEnd - frameStart);
+			if (sleepTime > std::chrono::microseconds{0}) {
+				std::this_thread::sleep_for(sleepTime);
+			}
+			else {
+				std::cerr << "DELAYES" << std::endl;
+			}
 		}
 		th.join();
 	}
